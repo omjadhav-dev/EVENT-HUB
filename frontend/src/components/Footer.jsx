5a1503f39b84768530db9b1cd961a3184004f7ca
omjadhav-dev <omsjadhav1991@gmail.com>
2026-08-20 21:52:22 +0530

    Footer update

--- frontend/src/components/Footer.jsx
+++ frontend/src/components/Footer.jsx
@@ -3,21 +3,21 @@ import { Link } from "react-router-dom";
 function Footer() {
   return (
     <div className="bg-gray-950 flex items-center justify-between px-6 py-4 text-white">
-      <Link to="/" className="font-bold text-xl text-white">
+      <Link to="/" className="font-bold text-xl text-white hover:text-yellow-400">
         EventHub*
       </Link>
 
-      <ul className="flex gap-5 cursor-pointer">
-        <li>Pricing</li>
-        <li>Help</li>
-        <li>Terms</li>
-        <li>Privacy</li>
+      <ul className="flex gap-5 cursor-pointer ">
+        <li className="hover:text-yellow-400">Pricing</li>
+        <li className="hover:text-yellow-400">Help</li>
+        <li className="hover:text-yellow-400">Terms</li>
+        <li className="hover:text-yellow-400">Privacy</li>
       </ul>
 
       <div className="flex gap-2">
-        <button className="rounded-full h-8 w-8 border">M</button>
-        <button className="rounded-full h-8 w-8 border">I</button>
-        <button className="rounded-full h-8 w-8 border">T</button>
+        <button className="rounded-full h-8 w-8 border hover:text-yellow-400">M</button>
+        <button className="rounded-full h-8 w-8 border hover:text-yellow-400">I</button>
+        <button className="rounded-full h-8 w-8 border hover:text-yellow-400">T</button>
       </div>
     </div>
   );
