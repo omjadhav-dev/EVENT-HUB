3a2d54975fb1085be1b08f86c9c6731df16895a4
omjadhav-dev <omsjadhav1991@gmail.com>
2026-08-20 21:02:24 +0530

    Frontend

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,4 +1,6 @@
 import { Routes, Route } from "react-router-dom";
+import { useEffect } from "react";
+import { useDispatch } from "react-redux";
 
 import PublicLayout from "./layouts/PublicLayout";
 import MainLayout from "./layouts/MainLayout";
@@ -10,10 +12,30 @@ import Explore from "./pages/Explore";
 import Event from "./pages/Event";
 import MyEvents from "./pages/MyEvents";
 import Create from "./pages/Create"
+import EditEvent from "./pages/EditEvent"
 import MyBookings from "./pages/MyBookings";
 import Profile from "./pages/Profile";
+import Analytics from "./pages/Analytics";
+
+import { getCurrentUser } from "./api/auth.api";
+import { login, authCheckFinished } from "./store/authSlice";
 
 function App() {
+  const dispatch = useDispatch();
+
+  // The JWT lives in an httpOnly cookie, so the frontend can't just read
+  // it - on every fresh page load we ask the backend "who am I?" (cookie
+  // gets sent automatically) to restore the logged-in state.
+  useEffect(() => {
+    getCurrentUser()
+      .then((res) => {
+        dispatch(login({ userData: res.data }));
+      })
+      .catch(() => {
+        dispatch(authCheckFinished());
+      });
+  }, [dispatch]);
+
   return (
     <Routes>
 
@@ -29,7 +51,9 @@ function App() {
         <Route path="/explore" element={<Explore />} />
         <Route path="/event/:id" element={<Event />}/>
         <Route path="/myevents" element={<MyEvents/>}/>
+        <Route path="/analytics" element={<Analytics/>}/>
         <Route path="/create" element={<Create/>}/>
+        <Route path="/myevents/:eventId/edit" element={<EditEvent/>}/>
         <Route path="/mybookings" element={<MyBookings/>}/>
         <Route path="/profile" element={<Profile/>}/>
       </Route>
--- frontend/src/main.jsx
+++ frontend/src/main.jsx
@@ -5,12 +5,15 @@ import { Provider } from "react-redux";
 import "./index.css";
 import App from "./App.jsx";
 import store from "./store/store.js";
+import { ToastProvider } from "./context/ToastContext.jsx";
 
 createRoot(document.getElementById("root")).render(
   <StrictMode>
     <Provider store={store}>
       <BrowserRouter>
-        <App />
+        <ToastProvider>
+          <App />
+        </ToastProvider>
       </BrowserRouter>
     </Provider>
   </StrictMode>,
